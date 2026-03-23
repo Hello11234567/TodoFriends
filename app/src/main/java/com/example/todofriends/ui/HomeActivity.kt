e4c9e307086db243427c3bc26007de9fd9a8539c
Hello11234567 <lia4031@naver.com>
2026-03-23 21:04:38 +0900

    Feat: 홈 화면 구현

--- app/src/main/java/com/example/todofriends/ui/HomeActivity.kt
+++ app/src/main/java/com/example/todofriends/ui/HomeActivity.kt
@@ -1,4 +1,32 @@
 package com.example.todofriends.ui
-class HomeActivity {
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.todofriends.R
+import com.kizitonwose.calendar.compose.HorizontalCalendar
+import com.kizitonwose.calendar.compose.rememberCalendarState
+import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import java.time.YearMonth
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstance: Bundle?) {
+        super.onCreate(savedInstance)
+        setContent {
+            HomeScreen()
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    val bgColor = Color(0xFF0F0F13)
 
 }
